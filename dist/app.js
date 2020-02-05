a3e1e08d5668b5bdc5c74c03505d2131ac6185a8
3KINGZ <victorkenneth95@gmail.com>
2020-02-05 03:03:56 +0100

    Added new words

--- dist/app.js
+++ dist/app.js
@@ -1,22 +1,3 @@
-//function to convert time into word;
-// const timeConvertToWord = () => {
-//     const hours = time.getHours();
-//     const minutes = time.getMinutes();
-
-//     if (minutes > 50) {
-//         wordTime = `${60 - minutes} MINUTES TO ${hours + 1}`;
-//     } else if (minutes > 0 && minutes < 30) {
-//         wordTime = `AFTER ${hours}`;
-//     } else if (minutes > 30) {
-//         wordTime = `${minutes} MINUTES PAST ${hours}`
-//     } else if (minutes === 0) {
-//         wordTime = `${hours}'o CLOCK`
-//     }
-//     let timeDisplay = document.createElement("h1"); //selecting and assigning values to DOM elements
-//     timeDisplay.textContent = wordTime;
-//     div.appendChild(timeDisplay);
-// }
-// timeConvertToWord();
 setInterval(function () {
     let time = new Date; //date object
     let div = document.querySelector("#time");
