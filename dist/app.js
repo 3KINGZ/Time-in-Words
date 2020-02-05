588b5571ad3ad06bf62b9088fa089b188ab8ca6a
3KINGZ <victorkenneth95@gmail.com>
2020-02-05 02:34:00 +0100

    modified timing update

--- dist/app.js
+++ dist/app.js
@@ -1,24 +1,39 @@
-let time = new Date; //date object
-let div = document.querySelector("#time");
-let wordTime;
-
 //function to convert time into word;
-const timeConvertToWord = () => {
-    const hours = time.getHours();
-    const minutes = time.getMinutes();
+// const timeConvertToWord = () => {
+//     const hours = time.getHours();
+//     const minutes = time.getMinutes();
 
+//     if (minutes > 50) {
+//         wordTime = `${60 - minutes} MINUTES TO ${hours + 1}`;
+//     } else if (minutes > 0 && minutes < 30) {
+//         wordTime = `AFTER ${hours}`;
+//     } else if (minutes > 30) {
+//         wordTime = `${minutes} MINUTES PAST ${hours}`
+//     } else if (minutes === 0) {
+//         wordTime = `${hours}'o CLOCK`
+//     }
+//     let timeDisplay = document.createElement("h1"); //selecting and assigning values to DOM elements
+//     timeDisplay.textContent = wordTime;
+//     div.appendChild(timeDisplay);
+// }
+// timeConvertToWord();
+setInterval(function () {
+    let time = new Date; //date object
+    let div = document.querySelector("#time");
+    let wordTime;
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let timeDisplay = document.createElement("h1"); //selecting and assigning values to DOM elements
+    div.innerHTML = "";
     if (minutes > 50) {
         wordTime = `${60 - minutes} MINUTES TO ${hours + 1}`;
-    } else if (minutes > 0 && minutes < 30) {
+    } else if (minutes > 0 && minutes <= 30) {
         wordTime = `AFTER ${hours}`;
     } else if (minutes > 30) {
         wordTime = `${minutes} MINUTES PAST ${hours}`
     } else if (minutes === 0) {
         wordTime = `${hours}'o CLOCK`
     }
-    let timeDisplay = document.createElement("h1"); //selecting and assigning values to DOM elements
     timeDisplay.textContent = wordTime;
     div.appendChild(timeDisplay);
-}
-timeConvertToWord();
-setTimeout(location.reload(), 120000); //calling the function each seconds
+}, 1000); //calling the function each seconds
